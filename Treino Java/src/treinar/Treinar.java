9c42893e8c99c0e58f80db9d97741500a566f672
MoisesCaris <99772321+MoisesCaris@users.noreply.github.com>
2023-03-13 21:13:30 -0300

    Add files via upload

---
+++ Treino Java/src/treinar/Treinar.java
@@ -0,0 +1,13 @@
+package treinar;
+
+public class Treinar {
+	public static void main(String[] args) {
+		System.out.print("xxxxxxxxxx");
+		System.out.print("\n"+"x  x  x  x");
+		System.out.print("\n"+"x        x");
+		System.out.print("\n"+"x x    x x");
+		System.out.print("\n"+"x  xxxx  x");
+		System.out.print("\n"+"xxxxxxxxxx");			
+	}   
+}       
+        
---
+++ Treino Java/src/treinar/Treinar_2.java
@@ -0,0 +1,29 @@
+package treinar;
+
+public class Treinar_2 {
+	public static void main(String[] args) {
+		System.out.println("        xxxx\n"
+				+ "        x   x\n"
+				+ "        xxxx\n"
+				+ "        x\n"
+				+ "        x	\n"
+				+ "xxxxxxxxxxxxxxxx\n"
+				+ " x            x\n"
+				+ "  x          x\n"
+				+ "   x        x\n"
+				+ "    xxxxxxxx");
+	}
+}
+/*      Um Barco
+        xxxx
+        x   x
+        xxxx
+        x
+        x	
+xxxxxxxxxxxxxxxx
+ x            x
+  x          x
+   x        x
+    xxxxxxxx
+
+*/
